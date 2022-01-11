575e0f936a5aaecdfd327b38d328e26bfbb4547b
crud0626 <sisofiy6048@gmail.com>
2022-01-11 15:36:33 +0900

    Add favicon

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,13 @@ import styles from './header.module.css';
 
 const Header = (props) => (
         <header>
-            <div className="center"></div>
+            <div className={styles.left}>
+                <img src="./images/mainlogo.png" alt="mainlogo" />
+                <h1>MD To HTML</h1>
+            </div>
+            <div className={styles.right}>
+
+            </div>
         </header>
     );
 
