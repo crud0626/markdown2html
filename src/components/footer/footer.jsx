5985e5c55b3f2d06c908594228e4691bf6219a85
crud0626 <sisofiy6048@gmail.com>
2022-01-13 21:58:46 +0900

    Complete css of header and footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,11 +5,17 @@ const Footer = (props) => (
         <footer>
             <div className={styles.footer_container}>
                 <div className="text">
-                    <span>Copyright. All right reversed Yeah!</span>
+                    <span>Copyright 2022. CRUD0626 All rights reserved.</span>
                 </div>
-                <div className="links">
-                    <a href="#" target="_blank" rel='noreferrer'>
-                        yeahyeah
+                <div className={styles.links_container}>
+                    <a 
+                        href="https://github.com/crud0626" 
+                        target="_blank" 
+                        rel='noopener noreferrer' 
+                        title='Github'
+                        className={styles.link_btn}
+                    >
+                            <img src="./images/GitHub-Mark-Light-32px.png" alt="github" />
                     </a>
                 </div>
             </div>
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,12 +3,16 @@ import styles from './header.module.css';
 
 const Header = (props) => (
         <header>
-            <div className={styles.left}>
-                <img src="./images/mainlogo.png" alt="mainlogo" />
-                <h1>MD To HTML</h1>
-            </div>
-            <div className={styles.right}>
-
+            <div className={styles.header_container}>
+                <div className={styles.left}>
+                    <img src="./images/mainlogo.png" alt="mainlogo" />
+                    <h1>MD TO HTML</h1>
+                </div>
+                <div className={styles.right}>
+                    <button className={styles.cheatsheet_btn}>
+                        CHEAT SHEET
+                    </button>
+                </div>
             </div>
         </header>
     );
