b5ab8bb5eed1f9ac4b6fca044c980ca260ad5db9
crud0626 <sisofiy6048@gmail.com>
2022-01-20 15:59:13 +0900

    Fixed bug related to deployment

--- src/components/buttons/buttons.jsx
+++ src/components/buttons/buttons.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import styles from './buttons.module.css';
 
 const Buttons = (props) => {
