815e65a79020b5e09c24e10fdf3a83fb25afe035
crud0626 <sisofiy6048@gmail.com>
2022-07-29 16:55:52 +0900

    Refactor: Add iconPath file for svg path
    * Add constants directory
    
    * Modify Input and Buttons components

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from 'react';
+import { eraserIcon } from '../../constants/iconPath';
 import styles from '../../styles/input.module.scss';
 import Icon from '../Icon/Icon';
 
@@ -63,9 +64,7 @@ const Input = (props: IProps) => {
             aria-label="input box"
         />
         <button id='eraser' className={styles.eraser_btn} onClick={props.onErase} aria-label="erase input button">
-            <Icon 
-                define={[`M56 884 c-9 -8 -16 -19 -16 -24 0 -9 247 -258 339 -343 22 -21 41 -42 41 -47 0 -5 -19 -26 -41 -47 -110 -101 -339 -335 -339 -346 0 -17 20 -37 37 -37 11 0 301 285 360 354 13 14 27 26 33 26 5 0 26 -19 47 -41 101 -110 335 -339 346 -339 17 0 37 20 37 37 0 15 -347 363 -362 363 -4 0 -8 14 -8 30 0 17 4 30 8 30 15 0 362 348 362 363 0 17 -20 37 -37 37 -11 0 -245 -229 -346 -339 -21 -22 -42 -41 -47 -41 -6 0 -20 12 -33 26 -48 56 -348 354 -357 354 -5 0 -16 -7 -24 -16z`]}
-            />
+            <Icon define={eraserIcon} />
         </button>
     </div>   
     );
